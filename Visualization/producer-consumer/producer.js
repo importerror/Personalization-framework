6b6faf7075d1d13234ee9b3af3306f96202b6d91
prometheansacrifice <prometheansacrifice@gmail.com>
2014-04-24 17:29:55 +0530

    transition added

--- Visualization/producer-consumer/producer.js
+++ Visualization/producer-consumer/producer.js
@@ -5,20 +5,19 @@ mongoose.connect('mongodb://localhost/producerConsumer');
 
 var VisitorModel = mongoose.model('visitor', { time: {type: Date}, count: {type: Number} });
 
-VisitorModel
-    .find()
-    .sort('time')
-    .limit(config.newVisitorCount)
-    .exec(function (err, d) {
-	d.forEach(function (d) {
-	    console.log('Old visitor removed');
-	    d.remove();
-	});
-    });
-
 var timeSlice = config.interval / (config.newVisitorCount + 1);
 
 setInterval(function () {
+    VisitorModel
+	.find()
+	.sort('time')
+	.limit(config.newVisitorCount)
+	.exec(function (err, d) {
+	    d.forEach(function (d) {
+		console.log('Old visitor removed');
+		d.remove();
+	    });
+	});
 
     var visitor = new VisitorModel({time: new Date(), count: parseInt(Math.random() * config.countLimit)});
 
